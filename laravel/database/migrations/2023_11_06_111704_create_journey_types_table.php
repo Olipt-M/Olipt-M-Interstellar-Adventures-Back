006eaf3177d45a3d459fd4ea57d479e8614f20a8
Paolo Dos Santos <paolo.a.dsts@gmail.com>
2023-11-06 15:06:45 +0100

    ships, users, journeys, journey_types, users_journeys, planets_journey_types tables created

--- laravel/database/migrations/2023_11_06_111704_create_journey_types_table.php
+++ laravel/database/migrations/2023_11_06_111704_create_journey_types_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('journey_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('base_price');
             $table->timestamps();
         });
     }
