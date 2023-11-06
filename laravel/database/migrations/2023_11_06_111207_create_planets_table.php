631e32839c5aaa5b77c11168d6fdc44b44c05b54
Olivier Mignot <olipt.mignot@gmail.com>
2023-11-06 15:39:53 +0100

    done migration + model for climates and planets

--- laravel/database/migrations/2023_11_06_111207_create_planets_table.php
+++ laravel/database/migrations/2023_11_06_111207_create_planets_table.php
@@ -14,6 +14,16 @@ return new class extends Migration
         Schema::create('planets', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 30);
+            $table->string('picture', 255);
+            $table->string('system', 50);
+            $table->string('distance_from_earth', 30);
+            $table->string('capital', 20)->nullable();
+            $table->smallInteger('date_colonization')->unsigned()->nullable();
+            $table->string('nb_inhabitants', 30)->nullable();
+            $table->unsignedBigInteger('climate_id');
+
+            $table->foreign('climate_id')->references('id')->on('climates');
         });
     }
 
