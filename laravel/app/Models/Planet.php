3ff67eaa4dccc0d24128d40fd3d3bf23e99ba10f
Paolo Dos Santos <paolo.a.dsts@gmail.com>
2023-11-06 16:52:17 +0100

    Planet Models modified

--- laravel/app/Models/Planet.php
+++ laravel/app/Models/Planet.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Planet extends Model
@@ -12,5 +13,9 @@ class Planet extends Model
 
     public function climate() : HasOne {
         return $this->hasOne(Climate::class);
+    }
+
+    public function journey() : BelongsTo {
+        return $this->belongsTo(Journey::class);
     }
 }
