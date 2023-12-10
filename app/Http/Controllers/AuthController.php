7934da19a0fc0b8b506eb8c517e7fc0dc775c826
Olivier <olipt.mignot@gmail.com>
2023-12-10 19:08:04 +0100

    cleaned AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,57 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RequestCreateUser;
 use App\Models\User;
-use App\Models\Journey;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    // Partie SignUp :
-
     public function register(RequestCreateUser $request) {
+        $user = new User($request -> all());
+        $user->save();
 
-    $user = new User($request -> all());
-    $user->save();
-
-    $token = $user->createToken('auth_token')->plainTextToken;
-    return response()->json([
-        'id' => $user['id'],
-        'firstname' => $user['firstname'],
-        'lastname' => $user['lastname'],
-        'email' => $user['email'],
-        'access_token' => $token,
-        'token_type' => 'Bearer',
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'id' => $user['id'],
+            'firstname' => $user['firstname'],
+            'lastname' => $user['lastname'],
+            'email' => $user['email'],
+            'access_token' => $token,
+            'token_type' => 'Bearer',
         ]);
-
     }
-
-    // Partie CONNEXION :
 
     public function login(Request $request){
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Mot de passe ou email non valide'], 401);
+        }
 
-    if (!Auth::attempt($request->only('email', 'password'))) {
-    return response()->json(['message' => 'Mot de passe ou email non valide'], 401);
-    }
+        else {
+            $user = User::where('email', 'like', $request['email'])->firstOrFail();
 
-    // if (Auth::attempt(['email' => $email, 'password' => $password], $remember)) {
-    //     return response()->json(['message' => 'Connexion reussie']);
-    // }
+            $token = $user->createToken('auth_token')->plainTextToken;
 
-    $user = User::where('email', $request['email'])->firstOrFail();
-
-    $token = $user->createToken('auth_token')->plainTextToken;
-
-    return response()->json([
-        'id' => $user['id'],
-        'firstname' => $user['firstname'],
-        'lastname' => $user['lastname'],
-        'email' => $user['email'],
-        'access_token' => $token,
-        'token_type' => 'Bearer',
-        ]);
+            return response()->json([
+                'id' => $user['id'],
+                'firstname' => $user['firstname'],
+                'lastname' => $user['lastname'],
+                'email' => $user['email'],
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }
     }
 
     public function me(Request $request){
